f80f0b6eca3ae01300c7c682948d89b48a6e89ee
Mika Jauhonen <mika@pinmeto.com>
2024-07-15 17:44:39 +0200

    For cube maps set flipY to false
    - Seems elm webgl dont reset it after use

--- src/Elm/Kernel/Texture.js
+++ src/Elm/Kernel/Texture.js
@@ -20,6 +20,7 @@ var _Texture_loadCube = F6(function (xPosUrl, xNegUrl, yPosUrl, yNegUrl, zPosUrl
     function createTexture(gl) {
       var texture = gl.createTexture();
       gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
       gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.xPos);
       gl.texImage2D(gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.xNeg);
       gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_Y, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, images.yPos);
